8fe74e0dd0b31757bc565516f633c3dc46a6c122
Ryong-Gyu Lee <dlfydrb@gmail.com>
2026-04-02 14:24:11 +0900

    Add electron density computation from density matrix

---
+++ lcao/compute/density.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+from lcao.core.model import phi_tolerance
+
+
+def _orbital_value_at_position(projector, io, position_vector, supercell_vectors):
+    atom_symbol = projector.atom_species[io]
+    atom_index = projector.atom_index[io] - 1
+
+    target_n = projector.orbital_n[io]
+    target_l = projector.orbital_l[io]
+    target_m = projector.orbital_ml[io]
+    target_z = projector.orbital_zeta[io]
+
+    target_position = projector.atoms[atom_index]
+
+    value = 0.0 + 0.0j
+    for vector in supercell_vectors:
+        xji = -(target_position - position_vector + vector)
+        radius = np.sqrt(xji.dot(xji))
+
+        phir = projector.Rnl(atom_symbol, target_n, target_l, target_z, radius)
+        if abs(phir) < phi_tolerance:
+            continue
+
+        spherical = projector.Yml(xji, target_m, target_l)
+        value += phir * spherical
+
+    return value
+
+
+def electron_density(projector, cell, mesh):
+    """Compute real-space electron density from the density matrix.
+
+    The density on each grid point is evaluated as
+    ``rho(r) = sum_{mu,nu} DM_{mu,nu} * phi_mu(r) * phi_nu(r)``.
+    """
+    projector.load_context(need_struct_supercell=True)
+    if not hasattr(projector, 'numh'):
+        projector._readHSX()
+
+    xgrid, ygrid, zgrid = projector.unit_cell_grid(cell, mesh)
+
+    na = int(mesh[0])
+    nb = int(mesh[1])
+    nc = int(mesh[2])
+
+    nbasis = projector.dm_nb
+    nspin = projector.dm_ns
+
+    rho = np.zeros((nspin, na, nb, nc), dtype=float)
+    supercell_vectors = projector._supercell_vector_list
+
+    for ix in range(na):
+        for iy in range(nb):
+            for iz in range(nc):
+                position_vector = np.array(
+                    [
+                        xgrid[0][ix][iy][iz],
+                        ygrid[0][ix][iy][iz],
+                        zgrid[0][ix][iy][iz],
+                    ],
+                    dtype=float,
+                )
+
+                phi = np.zeros((nbasis), dtype=np.complex128)
+                for io in range(nbasis):
+                    phi[io] = _orbital_value_at_position(projector, io, position_vector, supercell_vectors)
+
+                for isp in range(nspin):
+                    density_value = 0.0
+                    for io1 in range(nbasis):
+                        row_start = projector.dm_listdptr[io1]
+                        row_end = row_start + projector.dm_numd[io1]
+                        for ind in range(row_start, row_end):
+                            io2 = projector.dm_listd[ind] - 1
+                            density_value += projector.dm[ind][isp] * (phi[io1] * phi[io2]).real
+
+                    rho[isp][ix][iy][iz] = density_value
+
+    projector.rho = rho
+    return rho
