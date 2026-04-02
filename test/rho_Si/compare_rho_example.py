d8b584905e172167ecd6d852bb54e0b6c29b352a
Ryong-Gyu Lee <dlfydrb@gmail.com>
2026-04-02 15:16:11 +0900

    Use local rho_Si file names in compare example

---
+++ test/rho_Si/compare_rho_example.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from lcao.io import read_rho
+from lcao4siesta import lcao
+
+io_cell, io_mesh, io_rho = read_rho("Si.RHO")
+
+model = lcao(
+    system="Si",
+    dm_file="Si.DM",
+    ion_files={"Si": "Si.ion"},
+)
+gen_rho = model.electron_density(io_cell, io_mesh)
+
+print("mesh", tuple(io_mesh), "shape", io_rho.shape)
+print("max|Δρ|", float(np.max(np.abs(gen_rho[0] - io_rho[0]))))
